e6da0d51b3e48b979c584eb0ee3f0144eb759242
LebogangMputle <lebozikalala@gmail.com>
2024-05-09 15:39:46 +0200

    Added the if statement to check if the dividend or divider is not a number

--- scripts.js
+++ scripts.js
@@ -7,3 +7,15 @@ form.addEventListener("submit", (event) => {
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
 });
+
+if (!Number.isFinite(dividend) || !Number.isFinite(divider)) {
+  // Add a class "critical-error" to the result element (likely changes background to red)
+  result.classList.add("critical-error");
+  // Display an error message in the result element
+  result.innerText = "Something critical went wrong. Please reload the page";
+  // Log an error message to the console for debugging
+  console.error("Error: Non-numeric input provided");
+  } else if (parseInt(divider) === 0) { // checiking if divider equal 0.
+  result.innerText = "Division not performed. Invalid number provided. Try again";
+  console.error("Error: Division by zero");
+}
